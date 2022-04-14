45ab041e7840da75d75a472269fcc34d1160f098
Mark Benjamin <mark@render.com>
2022-04-14 16:54:42 -0400

    Adding concurrent and non-concurrent cases with standard dd-trace

--- example.test.js
+++ example.test.js
@@ -1,5 +1,14 @@
 const trace = require('dd-trace')
+const axios = require('axios')
 
-test.concurrent("Example test case", () => {
+test.concurrent("Concurrent example test case", async () => {
+    await axios.default.get("https://google.com")
+
+    expect(trace.scope().active()).not.toBeNull();
+})
+
+test("Nonconcurrent example test case", async () => {
+    await axios.default.get("https://google.com")
+
     expect(trace.scope().active()).not.toBeNull();
 })
